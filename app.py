a78d6b362bfa4a1cab84c0dedc44a77f771215cf
medyanikroman2017-ship-it <medyanik.roman2017@gmail.com>
2026-04-20 10:32:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ print("🚀 NEW VERSION LOADED:", time.time())
 
 # ===== CONFIG =====
 PENDING_TTL = 1200
-BIG_STORE_THRESHOLD = 1200
+BIG_STORE_THRESHOLD = 1500
 SMALL_STORE_THRESHOLD = 600
 
 r = redis.from_url(os.environ.get("REDIS_URL"), decode_responses=True)
